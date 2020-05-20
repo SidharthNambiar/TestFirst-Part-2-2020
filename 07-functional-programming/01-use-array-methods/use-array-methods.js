e6b9a37937a8c2c9d22c6276575016018d524c1b
Sidharth Nambiar <snambiar01@gmail.com>
2020-05-20 02:37:34 -0400

    test first part 2 complete

--- 07-functional-programming/01-use-array-methods/use-array-methods.js
+++ 07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -58,3 +58,95 @@ function getPopulation(arrOfCountries, arrOfSelectCountries) {
 }
 
 
+function keyifyArrayOfObjects(key, superheroes) {
+
+    let result = superheroes.reduce((finalObj, currentObj) => {
+        finalObj[currentObj[key]] = currentObj;
+        return finalObj;
+
+    }, {})
+
+    return result;
+
+}
+
+function powerLevelAverage(superheroes) {
+
+    let numberOfSuperheroes = superheroes.length;
+
+    let totalPower = superheroes.reduce((totalPowerLevel, curretSuperhero) => {
+        totalPowerLevel += curretSuperhero.powerLevel;
+        return totalPowerLevel;
+
+    }, 0)
+
+    let averagePower = totalPower / numberOfSuperheroes;
+
+    return Math.round(averagePower);
+
+}
+
+function mapReduce(arr, callback) {
+
+    let result = arr.reduce((finalArr, currentElement) => {
+
+        finalArr.push(callback(currentElement));
+        return finalArr;
+
+    }, [])
+
+    return result;
+}
+
+function filterReduce(arr, callback) {
+
+    let result = arr.reduce((finalArr, currentElement) => {
+        if (callback(currentElement)) finalArr.push(currentElement)
+        return finalArr;
+
+    }, [])
+
+    return result;
+}
+
+function inYourBudget(maxPrice, items) {
+
+    let result = items.filter((item) => {
+        return item.price <= maxPrice;
+    }).map((item) => {
+        return item.item
+    })
+
+    return result;
+}
+
+function separateAndReturnNames(superheroes, firstOrLast, maxLengthOfName) {
+
+    let result = superheroes.map((superhero) => {
+        let splitName = superhero['name'].split(' ')
+        superhero['firstName'] = splitName[0];
+        superhero['lastName'] = splitName[1];
+
+        return superhero;
+    }).filter((superhero) => {
+        return superhero[firstOrLast].length <= maxLengthOfName;
+    }).map((superhero) => {
+        return superhero[firstOrLast]
+    })
+
+    console.log(result)
+    return result;
+}
+
+function priorityTodoDuration(todoList) {
+
+    let result = todoList.filter((task) => {
+        return task.priority === 'high';
+    }).reduce((totalMinutes, task) => {
+        totalMinutes += task.duration;
+        return totalMinutes;
+
+    }, 0)
+
+    return result;
+}
